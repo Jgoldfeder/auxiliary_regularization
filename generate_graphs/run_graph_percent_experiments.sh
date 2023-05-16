e844c2d51bbd5d235ab852892860b02ebc50bd71
Gabe <gzg2104@columbia.edu>
2023-05-16 14:41:13 -0400

    have all of the datasets in graph percent experimetns

--- generate_graphs/run_graph_percent_experiments.sh
+++ generate_graphs/run_graph_percent_experiments.sh
@@ -1,4 +1,4 @@
-for i in 'birds' 'flowers' 'dogs' 'pets' 'cifar10' 'cifar100' 'aircraft'
+for i in 'caltech' 'birds' 'flowers' 'dogs' 'pets' 'cifar10' 'cifar100' 'aircraft'
 do
     python3 graph_percent_experiments.py --dataset "${i}" --levels "1 2 5 10 25" --metric_name "Validation Accuracy"
 done
